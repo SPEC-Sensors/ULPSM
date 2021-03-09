5ddf8b321621c82f1704a3ca2c6210f502dc4349
David Peaslee <dpeaslee@spec-sensors.com>
2021-03-09 10:52:19 -0600

    Update ULP.cpp
    Updated the .cpp for better use of temperature compensation

--- src/ULP.cpp
+++ src/ULP.cpp
@@ -1,250 +1,230 @@
 /*
   ULP.cpp - Library for reading SPEC Sensors ULP.
-  Created by David E. Peaslee, OCT 27, 2017.
+  Revised by David E. Peaslee, May 22, 2020.
+  Created by David E. Peaslee, OCT 27, 2016.
   Released into the public domain.
 */
 
 #include "Arduino.h"
 #include "ULP.h"
 
-//#define DEBUG
-#ifdef DEBUG
-	#define DEBUG_PRINT(x)     Serial.print (x)
-#else 
-	#define DEBUG_PRINT(x)
-#endif
+ULP::ULP (int a, int b, float c) : pCPin(a), pTPin(b), pSf(c) {
 
-
-
-ULP::ULP (int a, int b, float c) : _CPin(a), _TPin(b), _Sf(c) {
-  _Htemp = 40.0;
-  _Ltemp = 20.0; //temps for cal of temp sensor
-  _Tb = 18.0;
-  _Ts = 87.0;
-  _Tz = 20.0;
-  _Hvolt = (_Htemp + _Tb) * _Vsup / _Ts;
-  _Lvolt = (_Ltemp + _Tb) * _Vsup / _Ts; //volts for cal of temp sensor
+  pTzero = 20.0;
+  pIzero = 0.0;
+  
+  //Temperature Sensor Settings
+  pHtemp = 40.0;
+  pLtemp = 20.0; //temps for cal of temp sensor
+  pTb = 18.0;     //temperature sensor coef
+  pTs = 87.0;     //temperature sensor coef
+  pHvolt = (pHtemp + pTb) * pVsup / pTs;  //volts for cal of temp sensor
+  pLvolt = (pLtemp + pTb) * pVsup / pTs; //volts for cal of temp sensor
 }
 
-float ULP::_Vcc = 5.0; 	//voltage for ADC conversion of analogRead()	
-float ULP::_Vsup = 3.3;	//voltage of ULP supply for thermister, etc.
+//float ULP::pVcc = 5.0;
+//float ULP::pVsup = 3.3;
 
-int  ULP::getTemp(int n, char* U) { //get temperature using n as time in seconds for averaging, and U for units "F"/"C"
+void ULP::getTemp(int n) {
   unsigned long etime, i = 0;
-  unsigned long AC = 0;
-  float temp;
+  unsigned long anaCounts = 0;
   etime = millis() + n * 1000;
-
   do {
-    AC = AC + analogRead(_TPin);
+    anaCounts = anaCounts + analogRead(pTPin);
     delay(1);
     i++;
   } while (millis() < etime);
-  
-  DEBUG_PRINT(AC);
-  DEBUG_PRINT(", ");
-  
-  float Cnts = float (AC) / float(i);
-  float Volts = Cnts * _Vcc / 1024.0;
+  float Cnts = float (anaCounts) / float(i);
+  float Volts = Cnts * pVcc / 1024.0;
 
-  temp = (_Ts / _Vsup) * Volts - _Tb;
+  pT = (pTs / pVsup) * Volts - pTb;
+  
+ 
+}
 
-  if (U == "F") {
-    return temp * 9 / 5 + 32;
+float ULP::convertT(char U){
+   if (U == 'F') {
+    float TempF = pT * 9 / 5 + 32;
+    return TempF;
   }
-  else if ( U == "C") {
-    return temp;
+  else if ( U == 'C') {
+    return pT;
   }
   else return 0;
 }
 
-void ULP::setTSpan(float t, String R) { //advanced function for setting high or low calibration factor for thermistor
-  Serial.print(F("Old temp. span and offset: "));
-  Serial.print(_Ts);
-  Serial.print(F(", "));
-  Serial.println(_Tb);
+float ULP::convertX(char U){
+  if (U == 'B')  return pX;
+  else if (U == 'M') return (pX/1000.0);
+  else return 0;
+}
+
+void ULP::setTSpan(float t, String R) {
+  Serial.print("Old temp. span and offset: ");
+  Serial.print(pTs);
+  Serial.print(", ");
+  Serial.println(pTb);
   unsigned long etime, i = 0, n = 10;
-  unsigned long AC = 0;
+  unsigned long anaCounts = 0;
   etime = millis() + n * 1000;
   do {
-    AC = AC + analogRead(_TPin);
+    anaCounts = anaCounts + analogRead(pTPin);
     delay(1);
     i++;
   } while (millis() < etime);
-  float Cnts = float (AC) / float(i);
-  float Volts = Cnts * _Vcc / 1024;
+  float Cnts = float (anaCounts) / float(i);
+  float Volts = Cnts * pVcc / 1024;
 
   if (R == "HIGH") {
-    _Htemp = t;
-    _Hvolt = Volts;
+    pHtemp = t;
+    pHvolt = Volts;
   }
   else if (R == "LOW") {
-    _Ltemp = t;
-    _Lvolt = Volts;
+    pLtemp = t;
+    pLvolt = Volts;
   }
-  _Ts = _Vsup * (_Htemp - _Ltemp) / (_Hvolt - _Lvolt);
-  _Tb = _Lvolt * (_Htemp - _Ltemp) / (_Hvolt - _Lvolt) - _Ltemp;
-  Serial.print(F("New temp. span and offset: "));
-  Serial.print(_Ts);
-  Serial.print(F(", "));
-  Serial.println(_Tb);
+  pTs = pVsup * (pHtemp - pLtemp) / (pHvolt - pLvolt);
+  pTb = pLvolt * (pHtemp - pLtemp) / (pHvolt - pLvolt) - pLtemp;
+  Serial.print("New temp. span and offset: ");
+  Serial.print(pTs);
+  Serial.print(", ");
+  Serial.println(pTb);
 }
 
-float  ULP::setVref (long R1, long R2, long R3, int bias) {	//function for setting vref for custom sensors. Not critical value as call to concentration cancels out _Vref with _Voff
-	if (bias>0){
-		_Vref = _Vsup * float(R1+R2) / float(R1 + R2 + R3) * 1000.0;
-	}
-	else if(bias<0) {
-		_Vref = _Vsup * float(R1) / float(R1 + R2 + R3) * 1000.0;
-	}
-	else return 0;
-  
-  return _Vref;
+void  ULP::setVref (int b, long R2) {
+  //Caluclate Expected Vref
+  if (b >= 0)  pVref = pVsup * float(R2 + 1000000) / float(R2 + 2000000) * 1000.0;
+  else  pVref = pVsup * float(1000000) / float(R2 + 2000000) * 1000.0;
+  pVref_set = pVref;
 }
 
-float  ULP::zero() { //function to zero sensor and get temperature at that zero _Tz
-  unsigned long etime, i = 0, n = 10;
-  unsigned long AC = 0;
-  etime = millis() + n * 1000;
-  do {
-    AC = AC + analogRead(_CPin);
-    delay(1);
-    i++;
-  } while (millis() < etime);
-  float Cnts = float (AC) / float(i);
-
-  float Vgas = Cnts * _Vcc * 1000.0 / 1024.0; //in mV
-  _Voff = Vgas - _Vref;
-  _Tz = getTemp(10);
-  return _Voff;
-} //returns voltage at zero concentration
-
-float ULP::getVgas(int n)	//gets the gas voltage from analog read. using n seconds as averaging time
-{
+bool  ULP::OCzero (int n) {
+  //Measure real Vref
   unsigned long etime, i = 0;
-  unsigned long AC = 0;
+  unsigned long anaCounts = 0;
+  Serial.println("Send any character when sensor is removed.");
+  while (Serial.available() <= 0) {}
+  Serial.println("Zeroing");
+  Serial.flush();
   etime = millis() + n * 1000;
   do {
-    AC = AC + analogRead(_CPin);
+    anaCounts = anaCounts + analogRead(pCPin);
     delay(1);
     i++;
   } while (millis() < etime);
-  float Cnts = float (AC) / float(i);
+  float Cnts = float (anaCounts) / float(i);
+  pVref_set = Cnts * pVcc * 1000.0 / 1024.0; //in mV
+      Serial.println(abs(pVref - pVref_set));
 
-  float Vgas = Cnts * _Vcc * 1000.0 / 1024.0; //in mV
-
-  return Vgas;
-} //returns voltage at zero concentration
-
-
-
-float  ULP::getConc(int n, float t) { //gets the concentration using n seconds as averaging time, with temperature correction if including t, otherwise, not temperature corrected.
-
-  float nA = (getVgas(n) - _Vref - _Voff)/1000.0/ float(_Gain) * 1000000000.0;
-    DEBUG_PRINT(nA);
-	DEBUG_PRINT(", ");
-  if (t>23){
-	nA = nA - _Ah*(t-_Tz); 
-  }
-  else{
-	nA = nA - _Al*(t-_Tz);
-  }
-  
-  float Conc = nA /_Sf*(1-_Tc*(t-_Tz));
- 
-  return Conc;
+  if (abs(pVref - pVref_set) > 50)return false;
+  else return true;
 }
 
-float ULP::setXSpan() { //advanced function for setting the new sensitivity factor when at 20C. Must be withing 10% of previous sensitivity.
+void  ULP::zero() {
+  pIzero = pInA;
+  pTzero = pT;
+} 
+
+void ULP::getIgas(int n){
+  unsigned long etime, i = 0;
+  unsigned long anaCounts = 0;
+  etime = millis() + n * 1000;
+  do {
+    anaCounts = anaCounts + analogRead(pCPin);
+    delay(1);
+    i++;
+  } while (millis() < etime);
+  float Cnts = float (anaCounts) / float(i);
+
+  pVgas = Cnts * pVcc * 1000.0 / 1024.0; //in mV
+  pInA = (pVgas-pVref_set)/pGain * 1000.0; 
+} 
+
+void  ULP::getConc(float t) {
+  float nA = pInA - pIzero * expI(t-pTzero);
+  float Sens = pSf * (1.0 + pTc * (t - 20.0));
+  pX = nA / Sens *1000.0;
+}
+
+float ULP::expI(float T){
+  return exp(T/pn);
+}
+
+void ULP::setXSpan() {
   Serial.setTimeout(10000);
   float X;
   float nA, Sf;
-  Serial.print(F("When gas concentration steady, enter Concentration in ppm followed by 'cr' = "));
+  Serial.print("When gas concentration steady, enter Concentration in ppm followed by 'cr' = ");
   while (Serial.available() <= 0) {}
   X = Serial.parseFloat();
   Serial.println(X);
-  nA = (getVgas(30) - _Vref - _Voff) / 1000.0 / _Gain * 1000000000;
-  Sf = nA / X;
-  if(abs(Sf-_Sf)*2/(Sf+_Sf)<.1){
-    _Sf = Sf;
+  getIgas(10);
+  
+  Sf = pInA / X;
+  if (abs(Sf - pSf) * 2 / (Sf + pSf) < .1) {
+    pSf = Sf;
   }
   else {
-    Serial.println(F("Error Setting Span"));
+    Serial.println("Error Setting Span");
   }
-  return _Sf;
 
 }
 
-EtOH::EtOH(int a, int b, float c) : ULP(a, b, c) {
-  setVref(1000000, 69800, 1000000, +1);
-  _Gain = 249000;
-  _Tc = 0.6;
-  _Ah = 22.5;
-  _Al = 1.7;
+EtOH::EtOH(int a, int b, float c) : ULP(a, b, c = 14.0) {
+  setVref(+100, 69800);
+  pGain = 249.0;
+  pn = 1;
+  pTc = 0.01;
 }
 
-H2S::H2S (int a, int b, float c): ULP(a, b, c) {
-  setVref(1000000, 2000, 1000000, +1);
-  _Gain = 49900;
-  _Tc = 0.15;
-  _Ah = 1.63;
-  _Al = 0.55;
+H2S::H2S (int a, int b, float c): ULP(a, b, c = 194.0) { //works
+  setVref(+3, 2000);
+  pGain = 49.9;
+  pn = -300.0;
+  pTc = 0.007;
 }
 
-CO::CO(int a, int b, float c) : ULP(a, b, c) {
-  setVref(1000000, 2000, 1000000, +1);
-  _Gain = 100000;
-  _Tc = 0.5;
-  _Ah = 3.91;
-  _Al = 1.11;
+CO::CO(int a, int b, float c) : ULP(a, b, c = 2.44) {
+  setVref(+3, 2000);
+  pGain = 100.0;
+  pn = 13.6;
+  pTc = 0.007;
 }
 
-IAQ::IAQ(int a, int b, float c) : ULP(a, b, c) {
-  setVref(1000000, 105000, 1000000, +1);
-  _Gain = 100000;
-  _Tc = 0.5;
-  _Ah = 16.20;
-  _Al = 4.61;
+IAQ::IAQ(int a, int b, float c) : ULP(a, b, c = 150.0) {
+  setVref(+150, 105000);
+  pGain = 100.0;
+  pn = 1;
+  pTc = 0.01;
 }
 
-SO2::SO2(int a, int b, float c) : ULP(a, b, c) {
-  setVref(1000000, 143000, 1000000, +1);
-  _Gain = 100000;
-  _Tc = 0.85;
-  _Ah = 0.41;
-  _Al = 0.14;
+SO2::SO2(int a, int b, float c) : ULP(a, b, c = 14.6) {
+  setVref(+200, 143000);
+  pGain = 100.0;
+  pn = 10.26;
+  pTc = -0.026;
 }
 
-NO2::NO2(int a, int b, float c) : ULP(a, b, c) {
-  setVref(1000000, 143000, 1000000, -1);
-  _Gain = 499000;
-  _Tc = 0.02;
-  _Ah = 0.02;
-  _Al = 0.01;
+NO2::NO2(int a, int b, float c) : ULP(a, b, c = -25.0) {
+  setVref(-25, 143000);
+  pGain = 499.0;
+  pn = 109.6;
+  pTc = 0.005;
 }
 
-RESP::RESP(int a, int b, float c) : ULP(a, b, c) {
-  setVref(1000000, 143000, 1000000, -1);
-  _Gain = 499000;
-  _Tc = 0.02;
-  _Ah = 0.99;
-  _Al = 0.26;
+RESP::RESP(int a, int b, float c) : ULP(a, b, c = -21.5) {
+  setVref(-200, 143000);
+  pGain = 499.0;
+  pn = 1;
+  pTc = 0.00;
 }
 
-O3::O3(int a, int b, float c) : ULP(a, b, c) {
-  setVref(1000000, 16200, 1000000, -1);
-  _Gain = 499000;
-  _Tc = 0.25;
-  _Ah = 0.04;
-  _Al = 0.02;
+O3::O3(int a, int b, float c) : ULP(a, b, c = -20.0) {
+  setVref(-25, 16200);
+  pGain = 499.0;
+  pn = 109.6;
+  pTc = -0.005;
 }
 
-SPEC::SPEC(int a, int b, float c): ULP(a, b, c) {
-	//must manually set gain, Tc, Ah, Al, and setVref
+SPEC::SPEC(int a, int b, float c): ULP(a, b, c = 1.0) {
 }
-
-
-
-
-
-
