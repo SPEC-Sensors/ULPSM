e15c26f6c708863d28fc98db719e81a06633fd59
David Peaslee <dpeaslee@spec-sensors.com>
2021-03-09 10:58:40 -0600

    Update ULPExample_1sensor.ino
    Updated code to provide better temperature compensation

--- examples/ULPExample_1sensor/ULPExample_1sensor.ino
+++ examples/ULPExample_1sensor/ULPExample_1sensor.ino
@@ -1,17 +1,10 @@
 /*
-  ULP - Library for reading SPEC Sensors ULP.
-  Created by David E. Peaslee, OCT 27, 2017.
-  Released into the SPEC Sensors domain.
+  ULP.cpp - Library for reading SPEC Sensors ULP.
+  Revised by David E. Peaslee, May 22, 2020.
+  Created by David E. Peaslee, OCT 27, 2016.
+  Released into the public domain.
 */
 
-/* The library is built to use several SPEC ULP sensors. An Arduino Uno can handle 3 sensors, since 2 analog pins are required for each sensor.
-  Functions include getTemp(seconds, C/F): returns temperature, setTSpan(seconds, "HIGH"/"LOW"): for calibrating temperature, setVref(R1,R2,R3) for custom sensors,
-  getVgas(seconds): for getting voltage of sensor, getConc(seconds, temperature in degC): for getting temperature corrected concentration,
-  setXSpan(): for setting the calibration factor with known concentration of gas. Variable include _Vcc: the voltage ref of the ADC, _Vsup: the voltage to the ULP, 
-  and _Gain the value of resistor R6. For more details see ULP.cpp in the libraries folder and the data sheet at http://www.spec-sensors.com/wp-content/uploads/2016/06/Analog-SDK-Users-Manual-v18.pdf
-*/  
-
-/* As an example, the ULP is connected this way: Vgas (pin1) to A0, Vtemp (Pin3) to A3, Gnd (Pin6) to Gnd, V+ (Pin7 or Pin8) to 3.3V of Arduino (must not be above 3.3V!!!).*/
 
 #include "Arduino.h"
 #include "ULP.h"
@@ -20,74 +13,104 @@
 
 const int C1 = A0;
 const int T1 = A3;
+//averaging times, keep these low, so that the ADC read does not overflow 32 bits. For example n = 5 reads ADC 4465 times which could add to 22bit number.
+const int n = 5; //number of seconds to read gas sensor
+const int m = 1; //number of seconds to read temperature sensor
+const int s = 10; //number of seconds to read all sensors should be greater than n+m+1
 
-const float Sf1 = 4.05; //nA/ppm replace this value with your own sensitivity
+const float Sf1 = 11.61; //nA/ppm change this to match your barcode!!!!
 
+unsigned long etime;
 
-float temp1;
-float TZero;
-float Vzero1;
-
-CO sensor1(C1, T1, Sf1);	//Sensor Types are EtOH, H2S, CO, IAQ, SO2, NO2, RESP, O3, and SPEC (custom)
-//O3 sensor2(C2, T2, Sf2);	//Example O3
-//H2S sensor3(C3, T3, Sf3);	//Example H2S
+SO2 sensor1(C1, T1, Sf1);  //Sensor Types are EtoH, H2S, CO, IAQ, SO2, NO2, RESP, O3, and SPEC (custom)
+//O3 sensor2(C2, T2, Sf2);  //Example O3
+//CO sensor3(C3, T3, Sf3);  //Example CO
 
 //  Include these if using different boards with different voltages
-//float ULP::_Vcc = 5.0;  //analogRead Reference Voltage
-//float ULP::_Vsup =3.3;  //voltage supplied to V+ of ULP, ideally 3.3 Volts 
+
 
 
 void setup() {
+  sensor1.pVcc = 5.04;  //analogRead Reference Voltage, maybe measure Aref??
+  sensor1.pVsup = 3.31;  //voltage supplied to V+ of ULP, default is 3.3 Volts, probably should measure this as well.
+
   Serial.flush();
   Serial.begin(9600);    // initialize serial communications at 9600 bps:
 
   Serial.println();
-  Serial.println("Setting Up");
+  Serial.println("Setting Up.");
 
   Serial.print("Vsup for all sensors = ");
-  Serial.println(ULP::_Vsup);
+  Serial.println(sensor1.pVsup);
   Serial.print("Vcc for all sensors = ");
-  Serial.println(ULP::_Vcc);
+  Serial.println(sensor1.pVcc);
   Serial.print("Vref for sensor 1 = ");
-  Serial.println(sensor1._Vref);
+  Serial.println(sensor1.pVref);
   
-  //  Using resistor values from board R1, R2, R3 are for setting _Vref and Bias, while R6 sets the gain
-  //  If using modified or custom boards set Vref and Gain like this
-  //long int R1 = 61900, R2 = 1000, R3 = 1000000;
-  //int bias = 1; //alternatively bias=-1; for negative bias.
-  //sensor1.setVref(R1, R2, R3, bias); //will set the new Vref for custom sensor voltage ladder. bias is necessary to set the correct arrangement
-  //sensor1._Gain = 49900; //resistor R6
 
-  //  Vref is not necessary if zero() is called for each sensor. If you already know the sensor zero you can comment this out, and set the zero with zero1 = measured mV.
-  Serial.print("Vzero = ");
-  Serial.println(Vzero1 = sensor1.zero());   //.zero() sets and returns the baseline voltage at current temperature with only clean air present
-  Serial.print("Tzero = ");
-  Serial.println(sensor1._Tz);
+  //  Using resistor values from board R1, R2, R3 are for setting pVref and Bias, while R6 sets the gain
+  //  If using modified or custom boards set Vref and Gain like this
+  //long int R2 = 1000;  //Assumes R1 and R3 are 1 MOhms in resistor ladder
+  //float bias = -25.0
+  //sensor2.setVref(bias, R2);
+  //sensor2.pGain = 100000; //resistor R6
+
+  //if you know the V_ref replace the following code...
+  Serial.println("Remove Sensor.");
+  if (sensor1.OCzero(n)) {
+    Serial.print("Vref new = ");
+    Serial.println(sensor1.pVref_set);
+  } else {
+    Serial.println("Recheck Settings, Zero out of range");
+    while (1) {
+      Serial.println(analogRead(A0));
+      delay(1000);
+      }
+  }
+  //...with this code and your measured value of new Vref
+  //sensor1.pVref_set = ????
   
-  
+
+
   //sensor1.setXSpan();                                //Must have previously zeroed in clean air, returns new span factor.
 
   //When calibrating the temperature use "LOW"/"HIGH" for the temperature range ie .setTSpan(40.2, "HIGH") where T is the current high temperature
   //sensor1.setTSpan((71 - 32.0) * 5.0 / 9.0, "LOW");
 
-  Serial.println("Finished Setting Up");
-  Serial.println("T1, mV1, C1");
 
+  Serial.println("Finished Setting Up, Replace Sensor Now.");
+  Serial.println("T1, mV, nA, C1");
+  etime = millis();
 }
 
 void loop() {
-  temp1 = sensor1.getTemp(1,"F");  // Use .getTemp(n, "F") to get temp in Fahrenheit, with n as int number of seconds for averaging and "F" or "C" for temp units
-    
-  Serial.print(temp1);
-  Serial.print(", ");
+  
+  while (Serial.available()) {
+    if (Serial.read() == 'Z') {
+      Serial.println("Zeroing");
+      sensor1.zero(); //Uses last values read of Izero and Tzero
+      Serial.print("Izero, Tzero: ");
+      Serial.print(sensor1.pIzero);
+      Serial.print(", ");
+      Serial.println(sensor1.pTzero);
+    }
+  }
 
-  //Use .getVgas(int n) where n is the number of seconds to average
-  //Use ._Vref to read the reference voltage (voltage offset)
-  Serial.print(sensor1.getVgas(1));
+  if (millis() - etime > s * 1000) {
+    etime = millis();
 
-  //Use .getConc(1, temp1) where temp1 is in deg C for temperature corrected span
-  Serial.print(", ");
-  Serial.println(sensor1.getConc(1,sensor1.getTemp(1)));
+    sensor1.getIgas(n);
+    sensor1.getTemp(m);
+    sensor1.getConc(sensor1.pT);
 
-  delay(100);
+    Serial.print(sensor1.convertT('C')); //use 'C' or 'F' for units
+    Serial.print(", ");
+    Serial.print(sensor1.pVgas);
+    Serial.print(", ");
+    Serial.print(sensor1.pInA);
+    Serial.print(", ");
+    Serial.println(sensor1.convertX('M')); //use 'M' or 'B' for units or Serial.println(sensor1.pX); to just print ppb
+
+  } else if (millis() - etime < 0)    etime = millis();
+
 }
